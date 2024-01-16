3042d0fb7b6e1a5bc456abeb928a8da1e3c4fb49
majimellow <150180142+majimellow@users.noreply.github.com>
2024-01-16 16:17:50 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Trigonometry Dash'
+copyright = '2023, Majimellow'
+author = 'Majimellow'
 
 release = '0.1'
 version = '0.1.0'
